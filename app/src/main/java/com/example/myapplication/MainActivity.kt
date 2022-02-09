47502d6b964a1bb5cd95654d7c8f172db91138c5
Oussama BOUMOUR <ouboumour@a106imac11.local.isima.fr>
2022-02-09 19:15:42 +0100

    toast

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
     class MainActivity : AppCompatActivity() {
 
@@ -16,14 +17,22 @@ import android.widget.TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "Je passe dans le onCreate")
+//        Log.d("MainActivity", "Je passe dans le onCreate")
         button = findViewById(R.id.button_validation)
         textView = findViewById(R.id.textView)
         editText = findViewById<EditText>(R.id.monPasswd)
 
+
+
         button?.setOnClickListener {
             val texteDeLEditText = editText?.text
             textView?.text = texteDeLEditText
+
+            val text = "Hello toast!"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
         }
     }
 }
