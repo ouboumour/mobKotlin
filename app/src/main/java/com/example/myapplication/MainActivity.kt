a9fe0cfeb7785e341f5b5534a1479fea2a819abe
Oussama BOUMOUR <ouboumour@a106imac11.local.isima.fr>
2022-02-09 18:51:27 +0100

    passwd field

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,14 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
     class MainActivity : AppCompatActivity() {
 
         var button : Button? = null
         var textView : TextView? = null
-        //var editText : TextView? = null
+        var editText : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +18,11 @@ import android.widget.TextView
 
         button = findViewById(R.id.button_validation)
         textView = findViewById(R.id.textView)
+        editText = findViewById<EditText>(R.id.monPasswd)
 
         button?.setOnClickListener {
-            textView?.text="cc"
+            val texteDeLEditText = editText?.text
+            textView?.text = texteDeLEditText
         }
     }
 }
